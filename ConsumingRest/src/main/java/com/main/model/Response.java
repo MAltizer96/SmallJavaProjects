d0b99aaaa677c688500d8d1faba7e32bd44e57af
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2022-05-03 19:27:51 -0400

    Deleted JokeList class and chagned to Response Class
    Response class takes in the embedded property to holds that list of objects

---
+++ ConsumingRest/src/main/java/com/main/model/Response.java
@@ -0,0 +1,30 @@
+package com.main.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+//@JsonIgnoreProperties(ignoreUnknown = true)
+public class Response {
+
+	@JsonProperty("_embedded")
+	private Embedded embedded;
+
+	public Embedded getEmbedded() {
+		return embedded;
+	}
+
+	public void setEmbedded(Embedded embedded) {
+		this.embedded = embedded;
+	}
+
+
+
+}
+
+
