b84682aeffc3bf43ac31faf713dbd10a2669e2ae
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2022-05-16 10:02:17 -0400

    commit

---
+++ SpringDataRest/bin/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user(username,password)values('steve','steve123');
+insert into user(username,password)values('matt','matt123');
+insert into user(username,password)values('mark','mark123');
