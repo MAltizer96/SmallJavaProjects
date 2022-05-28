2aefa2e2aa698b39d22093072315ecbefada31ec
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2022-05-28 15:44:41 -0400

    Update UserRepo.java

--- SpringDataRest/src/main/java/com/user/repo/UserRepo.java
+++ SpringDataRest/src/main/java/com/user/repo/UserRepo.java
@@ -1,14 +1,9 @@
 package com.user.repo;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.user.model.User;
 
-@RepositoryRestResource(collectionResourceRel = "users", path = "users")
-public interface UserRepo extends JpaRepository<User, Integer> {
-	public List<User> findByUsername(@RequestParam("username") String username);
+public interface UserRepo extends JpaRepository<User,Long>  {
+
 }
